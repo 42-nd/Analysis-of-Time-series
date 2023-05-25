a1a438cc023b4a8e09b088374b422066bf1ec642
42-nd <rtrt8682@gmail.com>
2023-05-25 09:37:37 +0700

    Made Y0, Y1, Y2  functions

--- satellite_model_0_order.py
+++ satellite_model_0_order.py
@@ -3,37 +3,73 @@ import matplotlib.pyplot as plt
 import scipy as sc
 import pandas as pd
 from SVD import hsvd
-PI = np.pi
-def f(X, tet):
-    ret = tet[0] + tet[1] * X + tet[2] * X**2 
-    for i in range(3,14,3):
-        ret += tet[i] * np.sin((2 * PI * X)/tet[i+1] + tet[i+2])
+
+
+def Y0(X, tet):
+    ret = tet[0] + tet[1] * X + tet[2] * X**2
+    for i in range(3, 14, 3):
+        ret += tet[i] * np.sin((2 * np.pi * X) / tet[i + 1] + tet[i + 2])  
+        # tet[i] = Bk, tet[i+1] = Pk, tet[i+2] = phik for 4 steps accordingly
     return ret
 
+
+def Y1(X, tet):
+    ret = tet[0] + tet[1] * X + tet[2] * X**2
+    for i in range(3, 14, 3):
+        ret += tet[i] * np.sin((2 * np.pi * X) / tet[i + 1]) + tet[i + 2] * np.cos((2 * np.pi * X) / tet[i + 1])  
+        # tet[i] = Сk, tet[i+1] = Pk, tet[i+2] = Dk for 4 steps accordingly
+    return ret
+
+
+def Y2(X, tet):
+    ret = tet[0] + tet[1] * X + tet[2] * X**2
+    for i in range(3, 14, 3):
+        ret += tet[i] * np.sin(X * tet[i + 1]) + tet[i + 2] * np.cos(X * tet[i + 1])  
+        # tet[i] = Сk, tet[i+1] = wk, tet[i+2] = Dk for 4 steps accordingly
+    return ret
+
+
 def loss_f(tet, X, y):
-    return np.sum((f(X, tet) - y)**2)
+    return np.sum((Y0(X, tet) - y) ** 2)
 
-df = pd.read_csv('D:\Github\Analysis-of-Time-series\Файлы данных РШВ с восстановленными пропусками(365 дней)\R01_365.csv', parse_dates=['ds'])[:5000]
-X = np.arange(df['ds'].shape[0])
-y = np.array(df['y'])*1e6
 
-window = 500    
+df = pd.read_csv(
+    "D:\Github\Analysis-of-Time-series\Файлы данных РШВ с восстановленными пропусками(365 дней)\R01_365.csv",
+    parse_dates=["ds"],
+)[:5000]
+X = np.arange(df["ds"].shape[0])
+y = np.array(df["y"]) * 1e6
+
+window = 500
 rank = 4
 l, h = hsvd(y, window, rank)
 y = l
-'''The zero-order method'''
-results = {}
-n_points = np.linspace(1, 10.0, num=3)
-for j in n_points:
-    tet = np.ones(15)*j
-    tet_pred = sc.optimize.minimize(loss_f, tet, args=(X, y), method='Powell').x
-    loss = loss_f(tet_pred, X, y)
-    results[loss] = [tet_pred]
 
-tet_pred = results[min(list(results.keys()))][0]
-print(tet_pred, loss_f(tet_pred, X, y))
-plt.plot(X,f(X,tet_pred),c='g')
-plt.plot(X,f(X,tet_pred)+h,c='black')
-plt.plot(X, np.array(df['y'])*1e6)
+tet_parameters = np.ones(15) * 7
+tet_pred_parameters_Y0 = sc.optimize.minimize(loss_f, tet_parameters, args=(X, y), method="Powell").x
+loss_Y0 = loss_f(tet_pred_parameters_Y0, X, y)
 
+print(tet_pred_parameters_Y0, loss_Y0, "LOSS Y0")
+plt.plot(X, Y0(X, tet_pred_parameters_Y0), c="g", label="Y0")
+
+
+tet_parameters = np.ones(15) * 7
+tet_pred_parameters_Y1 = sc.optimize.minimize(loss_f, tet_parameters, args=(X, y), method="Powell").x
+loss_Y1 = loss_f(tet_pred_parameters_Y1, X, y)
+
+print(tet_pred_parameters_Y1, loss_Y1, "LOSS Y1")
+plt.plot(X, Y1(X, tet_pred_parameters_Y1), c="pink", label="Y1")
+
+
+# tet_parameters = np.ones(15) * 7
+# tet_pred_parameters_Y2 = sc.optimize.minimize(loss_f, tet_parameters, args=(X, y), method="Powell").x
+# loss_Y2 = loss_f(tet_pred_parameters_Y2, X, y)
+
+# print(tet_pred_parameters_Y2, loss_Y2, "LOSS Y2")
+# plt.plot(X, Y2(X, tet_pred_parameters_Y2), c="black", label="Y2")
+
+
+
+plt.plot(X, np.array(df["y"]) * 1e6, label="Original data")
+plt.legend()
 plt.show()
